2c0febf12d9f1b5a9c807222954201671e8622d4
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-05-22 05:42:16 +0700

    first commit

---
+++ lib/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color(0XFF5843BE);
+Color secondaryColor = Color(0XFFFF9376);
+Color greySubTextColor = Color(0XFF7A7E86);
+Color lightGreySubTextColor = Color(0XFF82868E);
