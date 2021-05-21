3bc2745a4d538a27be8a342a849426ce98ed8257
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-05-22 06:49:18 +0700

    Splash - Footer (finish).

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -6,62 +6,85 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.only(top: 50, left: 30, right: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 50,
-                width: 50,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                      'assets/images/logo.png',
+        bottom: false,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset('assets/images/splash_image.png'),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 50, left: 30, right: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/images/logo.png',
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Text(
-                'Find Cozy House\nto Stay and Happy',
-                style: titleTextStyle.copyWith(fontSize: 24),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Stop membuang banyak waktu\npada tempat yang tidak habitable',
-                style: subTitleTextStyle.copyWith(
-                  fontSize: 16,
-                  color: lightGreySubTextColor,
-                ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                width: 210,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Explore Now',
-                    style: btnTextStyle,
+                  SizedBox(
+                    height: 15,
                   ),
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.all(11),
-                    primary: primaryColor,
-                    shape: RoundedRectangleBorder(
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white70,
                       borderRadius: BorderRadius.circular(17),
                     ),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 15, bottom: 15, right: 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Find Cozy House\nto Stay and Happy',
+                            style: titleTextStyle.copyWith(fontSize: 24),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'Stop membuang banyak waktu\npada tempat yang tidak habitable',
+                            style: subTitleTextStyle.copyWith(
+                              fontSize: 16,
+                              color: lightGreySubTextColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Container(
+                    width: 210,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Explore Now',
+                        style: btnTextStyle,
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(11),
+                        primary: primaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(17),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
