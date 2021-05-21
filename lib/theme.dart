a222231e4824a65af30917020ab4a1ab6e7b5540
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-05-22 05:55:14 +0700

    Finishing Theme.

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color primaryColor = Color(0XFF5843BE);
 Color secondaryColor = Color(0XFFFF9376);
 Color greySubTextColor = Color(0XFF7A7E86);
 Color lightGreySubTextColor = Color(0XFF82868E);
+
+TextStyle titleTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: Colors.black,
+);
+
+TextStyle subTitleTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+);
+
+TextStyle btnTextStyle = GoogleFonts.poppins(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  color: Colors.white,
+);
+
+TextStyle accentTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: primaryColor,
+);
