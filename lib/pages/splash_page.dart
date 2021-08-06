a168cd5be0273246543d5ec6f18e04304bb68d49
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-08-06 22:33:11 +0700

    Splash page responsive update.

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,6 +1,7 @@
 import 'package:cozy_app/pages/home_page.dart';
 import 'package:cozy_app/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashPage extends StatelessWidget {
   @override
@@ -13,16 +14,31 @@ class SplashPage extends StatelessWidget {
           children: [
             Align(
               alignment: Alignment.bottomCenter,
-              child: Image.asset('assets/images/splash_image.png'),
+              child: Container(
+                height: 289.h,
+                width: double.infinity,
+                color: secondaryColor,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Image.asset(
+                'assets/images/house.png',
+                height: 433.h,
+              ),
             ),
             Padding(
-              padding: EdgeInsets.only(top: 50, left: 30, right: 30),
+              padding: EdgeInsets.only(
+                top: 50.h,
+                left: 30.w,
+                right: 30.w,
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    height: 50,
-                    width: 50,
+                    height: 50.r,
+                    width: 50.r,
                     decoration: BoxDecoration(
                       image: DecorationImage(
                         image: AssetImage(
@@ -32,29 +48,30 @@ class SplashPage extends StatelessWidget {
                     ),
                   ),
                   SizedBox(
-                    height: 15,
+                    height: 15.h,
                   ),
                   Container(
                     decoration: BoxDecoration(
                       color: Colors.white70,
-                      borderRadius: BorderRadius.circular(17),
+                      borderRadius: BorderRadius.circular(17.w),
                     ),
                     child: Padding(
-                      padding: EdgeInsets.only(top: 15, bottom: 15, right: 15),
+                      padding:
+                          EdgeInsets.only(top: 15.r, bottom: 15.r, right: 15.r),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
                             'Find Cozy House\nto Stay and Happy',
-                            style: titleTextStyle.copyWith(fontSize: 24),
+                            style: titleTextStyle.copyWith(fontSize: 24.sp),
                           ),
                           SizedBox(
-                            height: 10,
+                            height: 10.h,
                           ),
                           Text(
                             'Stop membuang banyak waktu\npada tempat yang tidak habitable',
                             style: subTitleTextStyle.copyWith(
-                              fontSize: 16,
+                              fontSize: 16.sp,
                               color: lightGreySubTextColor,
                             ),
                           ),
@@ -63,30 +80,29 @@ class SplashPage extends StatelessWidget {
                     ),
                   ),
                   SizedBox(
-                    height: 25,
+                    height: 25.h,
                   ),
-                  Container(
-                    width: 210,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => HomePage(),
-                          ),
-                        );
-                      },
-                      child: Text(
-                        'Explore Now',
-                        style: btnTextStyle,
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.all(11),
-                        primary: primaryColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(17),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomePage(),
                         ),
+                      );
+                    },
+                    child: Text(
+                      'Explore Now',
+                      style: btnTextStyle,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 11.r,
+                        horizontal: 50.r,
+                      ),
+                      primary: primaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(17.w),
                       ),
                     ),
                   ),
