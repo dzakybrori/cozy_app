334e5608f05a8f4adfb32ffccec74de61f854027
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-08-06 17:08:51 +0700

    Set Device Orientaion and Change Icon Launcher.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cozy_app/pages/splash_page.dart';
 import 'package:cozy_app/provider/space_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
@@ -12,6 +13,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // Set Device Orientation
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      // DeviceOrientation.portraitDown,
+    ]);
     return ScreenUtilInit(
       designSize: Size(375, 812),
       builder: () => ChangeNotifierProvider(
