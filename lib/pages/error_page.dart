0c35d7b6cef4ecb62a9ea2399390f3bc1774a5af
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-08-07 15:05:30 +0700

    Error Page finished responsive.

--- lib/pages/error_page.dart
+++ lib/pages/error_page.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:cozy_app/pages/home_page.dart';
 import 'package:cozy_app/theme.dart';
-import 'package:flutter/material.dart';
 
 class ErrorPage extends StatelessWidget {
   @override
@@ -10,7 +11,7 @@ class ErrorPage extends StatelessWidget {
       body: SafeArea(
         bottom: false,
         child: Padding(
-          padding: EdgeInsets.all(38),
+          padding: EdgeInsets.all(38.w),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -19,28 +20,28 @@ class ErrorPage extends StatelessWidget {
                 width: MediaQuery.of(context).size.width,
               ),
               SizedBox(
-                height: 70,
+                height: 70.h,
               ),
               Text(
                 'Where are you going?',
                 style: titleTextStyle.copyWith(
-                  fontSize: 24,
+                  fontSize: 24.sp,
                 ),
                 textAlign: TextAlign.center,
               ),
               SizedBox(
-                height: 14,
+                height: 14.h,
               ),
               Text(
                 'Seems like the page that you were\nrequested is already gone',
                 style: subTitleTextStyle.copyWith(
-                  fontSize: 16,
+                  fontSize: 16.sp,
                   color: lightGreySubTextColor,
                 ),
                 textAlign: TextAlign.center,
               ),
               SizedBox(
-                height: 50,
+                height: 50.h,
               ),
               ElevatedButton(
                 onPressed: () {
@@ -54,8 +55,8 @@ class ErrorPage extends StatelessWidget {
                 },
                 child: Padding(
                   padding: EdgeInsets.symmetric(
-                    horizontal: 42,
-                    vertical: 12,
+                    horizontal: 42.w,
+                    vertical: 12.h,
                   ),
                   child: Text(
                     'Back to Home',
@@ -65,7 +66,7 @@ class ErrorPage extends StatelessWidget {
                 style: ElevatedButton.styleFrom(
                   primary: primaryColor,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(17),
+                    borderRadius: BorderRadius.circular(17.w),
                   ),
                 ),
               ),
