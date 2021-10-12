7813daf1567d240348eb6ace55d2a7647f6d244e
dzakybrori <broridzaky2702@gmail.com>
2021-10-13 01:34:58 +0700

    Optimize Space Provider.

--- lib/provider/space_provider.dart
+++ lib/provider/space_provider.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'dart:convert';
 
 import 'package:cozy_app/models/space.dart';
@@ -5,19 +6,30 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class SpaceProvider with ChangeNotifier {
+  List<Space> _items = [];
+
+  UnmodifiableListView<Space> get items => UnmodifiableListView(_items);
+
+  UnmodifiableListView<Space> get favItems =>
+      UnmodifiableListView(_items.where((element) => element.isFavorite));
+
   Future<List<Space>> getRecommendedSpaces() async {
-    var result = await http.get(
-      Uri.parse('https://bwa-cozy.herokuapp.com/recommended-spaces'),
-    );
+    if (_items.isEmpty) {
+      var result = await http.get(
+        Uri.parse('https://bwa-cozy.herokuapp.com/recommended-spaces'),
+      );
 
-    print(result.statusCode);
+      print(result.statusCode);
 
-    if (result.statusCode == 200) {
-      List data = jsonDecode(result.body);
-      List<Space> spaces = data.map((item) => Space.fromJson(item)).toList();
-      return spaces;
+      if (result.statusCode == 200) {
+        List data = jsonDecode(result.body);
+        List<Space> spaces = data.map((item) => Space.fromJson(item)).toList();
+        return spaces;
+      } else {
+        return <Space>[];
+      }
     } else {
-      return <Space>[];
+      return items;
     }
   }
 }
