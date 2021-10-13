b905458d9256cf7ee9a550dcffcc73a6de2751ee
dzakybrori <broridzaky2702@gmail.com>
2021-10-13 16:18:02 +0700

    Migrate Error Page Finish.

--- lib/pages/error_page.dart
+++ lib/pages/error_page.dart
@@ -1,72 +1,52 @@
-import 'package:cozy_app/shared/shared_value.dart';
-import 'package:cozy_app/shared/theme.dart';
+import 'package:cozy_app/widgets/my_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../extension/extensions.dart';
+import '../shared/shared_value.dart';
 
 class ErrorPage extends StatelessWidget {
-  const ErrorPage({Key? key}) : super(key: key);
+  final String? errorTitle;
+  final String? errorSubTitle;
+  final bool useButton;
+
+  const ErrorPage({Key? key, this.errorTitle, this.errorSubTitle, this.useButton = true})
+      : super(key: key);
 
   void _onClickBackHome(BuildContext context) =>
       Navigator.popUntil(context, ModalRoute.withName(RouteName.homePage));
 
+  String _errorTitle() => errorTitle ?? 'Where are you going?';
+
+  String _errorSubTitle() =>
+      errorSubTitle ??
+      'Seems like the page that you were\nrequested is already gone';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       body: SafeArea(
         bottom: false,
         child: Padding(
-          padding: EdgeInsets.all(38.w),
+          padding: EdgeInsets.only(
+            top: context.h(14),
+            left: context.dp(38),
+            right: context.dp(38),
+            bottom: context.h(36),
+          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset(
-                'assets/images/error_404.png',
-                width: MediaQuery.of(context).size.width,
-              ),
-              SizedBox(
-                height: 70.h,
-              ),
-              Text(
-                'Where are you going?',
-                style: titleTextStyle.copyWith(
-                  fontSize: 24.sp,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 14.h,
-              ),
-              Text(
-                'Seems like the page that you were\nrequested is already gone',
-                style: subTitleTextStyle.copyWith(
-                  fontSize: 16.sp,
-                  color: lightGreySubTextColor,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 50.h,
-              ),
-              ElevatedButton(
-                onPressed: () => _onClickBackHome(context),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 42.w,
-                    vertical: 12.h,
-                  ),
-                  child: Text(
-                    'Back to Home',
-                    style: btnTextStyle,
-                  ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  primary: primaryColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(17.w),
-                  ),
-                ),
-              ),
+              Image.asset('assets/images/error_404.png', width: context.dw),
+              SizedBox(height: context.dp(70)),
+              MyText(_errorTitle(),
+                  style: context.text.headline6, textAlign: TextAlign.center),
+              SizedBox(height: context.dp(14)),
+              Text(_errorSubTitle(),
+                  style: context.text.subtitle1, textAlign: TextAlign.center),
+              if(useButton) SizedBox(height: context.dp(50)),
+              if(useButton) ElevatedButton(
+                  onPressed: () => _onClickBackHome(context),
+                  child: MyText('Back to Home')),
             ],
           ),
         ),
