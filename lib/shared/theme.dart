7528b22984d426f8eb7d3dff9fbd934728cf4e88
dzakybrori <broridzaky2702@gmail.com>
2021-10-14 00:22:42 +0700

    Remove unnecessary package.

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,7 +1,6 @@
-import 'package:cozy_app/shared/shared_method.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
+
+import './shared_method.dart';
 
 Color primaryColor = Color(0XFF5843BE);
 Color secondaryColor = Color(0XFFFF9376);
@@ -13,29 +12,3 @@ Color lightGreySubTextColor = Color(0XFF82868E);
 
 MaterialColor primarySwatch = generateMaterialColor(primaryColor);
 MaterialColor secondarySwatch = generateMaterialColor(secondaryColor);
-
-TextStyle regulerTextStyle = GoogleFonts.poppins(
-  fontSize: 16.sp,
-  fontWeight: FontWeight.w400,
-  color: Colors.black,
-);
-
-TextStyle titleTextStyle = GoogleFonts.poppins(
-  fontWeight: FontWeight.w500,
-  color: Colors.black,
-);
-
-TextStyle subTitleTextStyle = GoogleFonts.poppins(
-  fontWeight: FontWeight.w300,
-);
-
-TextStyle btnTextStyle = GoogleFonts.poppins(
-  fontSize: 18.sp,
-  fontWeight: FontWeight.w500,
-  color: Colors.white,
-);
-
-TextStyle accentTextStyle = GoogleFonts.poppins(
-  fontWeight: FontWeight.w500,
-  color: primaryColor,
-);
